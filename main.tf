7a5e94960c000a5c319c7a334ffe1d05d5d38b50
Lokesh <loksdoss8055@gmail.com>
2026-03-27 17:12:09 +0530

    configured remote backend

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "testdemotfstate"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
 provider "azurerm" {
   features {}
 }
